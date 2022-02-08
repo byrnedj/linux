69534e97c972814344229c5813c222a13e8b5ca9
Sanjay Kumar <sanjay.k.kumar@intel.com>
2022-02-07 18:35:17 -0800

    dmaengine: idxd: Implement the new kernel to user copy functions
    Implement the new dmaengine kernel to user copy functions with PASID
    support
    TODO: DSA2 desc submission
    
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

--- drivers/dma/idxd/idxd.h
+++ drivers/dma/idxd/idxd.h
@@ -188,6 +188,12 @@ struct idxd_dma_chan {
 	struct idxd_wq *wq;
 
 	struct list_head pending;
+	u32 num_queued;
+
+	/* for KERNEL_USER DMA Channels */
+	int pasid;
+	bool priv;
+	bool pasid_valid;
 };
 
 struct idxd_wq {
