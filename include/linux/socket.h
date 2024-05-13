541b148cc6bdd1cb82d3e5288596025f02c28526
Nikhil Rao <nikhil.rao@intel.com>
2024-05-13 01:54:57 -0700

    io_recv dma
    from https://github.com/intel-sandbox/io_uring_dsa_app/tree/main/recv
    
    ../setup_dsa.sh -d dsa0 -w1 -md
    ./main  -t 20
    
    tools/testing/selftests/net/udpgso_bench_tx -D 127.0.0.1 -4 -C 2 -s 1472 -u
    
    BW reported by the recv program is approx 1GB

--- include/linux/socket.h
+++ include/linux/socket.h
@@ -90,6 +90,7 @@ struct msghdr {
 	unsigned int	msg_flags;	/* flags on received message */
 	__kernel_size_t	msg_controllen;	/* ancillary data buffer length */
 	struct kiocb	*msg_iocb;	/* ptr to iocb for async requests */
+	struct io_kiocb *msg_io_iocb;
 	struct ubuf_info *msg_ubuf;
 	int (*sg_from_iter)(struct sk_buff *skb,
 			    struct iov_iter *from, size_t length);
